b1c23502a1a8b7cefe1e1b5b891099dcff1a568f
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-11 09:43:43 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './componants/Home';
 import Contact from './componants/Contact';
 import Footer from './componants/Footer';
 import Affiches from './componants/Affiches';
+import MentionsLegales from './componants/MentionsLegales';
+import Films from './componants/Films';
 
 function App() {
   return (
@@ -12,8 +14,10 @@ function App() {
       <NavBar />
       <Routes>
         <Route path= "/" element={<Home />} />
+        <Route path= "/films" element={<Films />} />
         <Route path= "/affiches" element={<Affiches />} />
         <Route path= "/contact" element={<Contact />} />
+        <Route path= "/mentionslegales" element={<MentionsLegales />} />
       </Routes>
       <Footer />
     </div>
--- src/componants/Footer.jsx
+++ src/componants/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <footer className='flex justify-between p-4'>
+    <footer className='text-center md:flex justify-between p-4 bg-slate-900 text-slate-100 md:text-lg md:p-6'>
+        <Link to="/mentionslegales" className=' hover:border-b-2'>Mentions Légales</Link>
         <p className="">
-          ©{new Date().getFullYear()} carole bethuel 
+            Copyright © {new Date().getFullYear()} carole bethuel. All rights reserved. 
         </p> 
-        <p className="">Mentions Légales</p>
     </footer>
   )
 }
