4c237d7eb444350e40ecea3fb7ad7338b7e9aa9c
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-16 15:49:47 +0100

    add affiches photos

--- src/componants/Affiches.jsx
+++ src/componants/Affiches.jsx
@@ -1,48 +1,48 @@
 import React from 'react';
-// import a1 from '../assets/a1.jpeg';
-// import a2 from '../assets/a2.jpeg';
-// import a3 from '../assets/a3.jpeg';
-// import a4 from '../assets/a4.jpeg';
-// import a5 from '../assets/a5.jpeg';
-// import a6 from '../assets/a6.jpeg';
+import a1 from '../assets/a1.jpeg';
+import a2 from '../assets/a2.jpeg';
+import a3 from '../assets/a3.jpeg';
+import a4 from '../assets/a4.jpeg';
+import a5 from '../assets/a5.jpeg';
+import a6 from '../assets/a6.jpeg';
 
 function Affiches() {
-    // const affiches = [
-    //     {
-    //         id: `name-1`,
-    //         img: a1,
-    //     },
-    //     {
-    //         id: `name-2`,
-    //         img: a2,
-    //     },
-    //     {
-    //         id: `name-3`,
-    //         img: a3,
-    //     },
-    //     {
-    //         id: `name-4`,
-    //         img: a4,
-    //     },
-    //     {
-    //         id: `name-5`,
-    //         img: a5,
-    //     },
-    //     {
-    //         id: `name-6`,
-    //         img: a6,
-    //     },
+    const affiches = [
+        {
+            id: `name-1`,
+            img: a1,
+        },
+        {
+            id: `name-2`,
+            img: a2,
+        },
+        {
+            id: `name-3`,
+            img: a3,
+        },
+        {
+            id: `name-4`,
+            img: a4,
+        },
+        {
+            id: `name-5`,
+            img: a5,
+        },
+        {
+            id: `name-6`,
+            img: a6,
+        },
 
-    // ]
+    ]
   return (
     <section>
         <h2 className="text-3xl text-center py-10">Affiches</h2>
 
         <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-           {/* {affiches.map((item) => (
+           {affiches.map((item) => (
             <li key={item.img} className="p-6 mx-auto">
                 <img src={item.img} alt="" className='rounded-2xl md:h-[32rem] w-full object-cover'/>
-            </li> */}
+            </li>
            ))} 
         </ul>
     </section>
--- src/componants/FilmDetails.jsx
+++ src/componants/FilmDetails.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Films() {
+  const [images, setImages] = useState ([
+    {id:1,
+    src: 'img1',
+    group: 'group1'},
+    {id:2,
+    src: 'img2',
+    group: 'group1'},
+    {id:3,
+    src: 'img3',
+    group: 'group2'},
+    {id:4,
+    src: 'img4',
+    group: 'group2'},
+
+  ]);
   return (
-    <div>Films</div>
+    <section>
+      <div className="">
+      {images
+          .filter(image => image.group === 'group1')
+          .map(image => (
+            <Link key={image.id} to={`/${image.id}`}>
+              <img src={image.src} alt={image.src} />
+            </Link>
+          ))}
+      </div>
+    </section>
   )
 }
 
