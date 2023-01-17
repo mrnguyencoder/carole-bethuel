59749351c32ad375c42fd447892c78159e8416b0
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-17 09:14:29 +0100

    text contact

--- src/componants/Contact.jsx
+++ src/componants/Contact.jsx
@@ -6,23 +6,23 @@ import imdb from '../assets/IMDB_Logo.png';
 export default function Contact() {
   return (
     <section className="h-screen">
-        <div className="flex flex-col justify-start items-center space-y-4 p-10">
-            <h2 className="text-4xl font-bold tracking-tight">Contact</h2>
+        <div className="flex flex-col justify-start items-center space-y-16 p-10">
+            <h2 className="text-4xl font-extrabold tracking-wider">Contact</h2>
             {/* <p className="mt-3 text-lg leading-6">Si vous avez des questions, des commandes spéciales,</p>  
             <p className='text-lg leading-6'>contactez-moi et je me ferai un plaisir de vous répondre.</p> */}
             <a href="tel:33782885224"
-                className="flex space-x-3 text-lg md:text-xl lg:text-2xl ">
+                className="flex space-x-3 text-xl md:text-2xl lg:text-4xl ">
                 <PhoneIcon className="md: h-8 flex-shrink-0 text-green-500" aria-hidden="true" />
                 <p className="">06 14 54 68 24</p>
             </a>
             <a href="mailto:carolebethuel@wanadoo.fr"
-                className="flex space-x-3 text-lg md:text-xl lg:text-2xl ">
+                className="flex space-x-3 text-xl md:text-2xl lg:text-4xl">
                 <EnvelopeIcon className="h-8 flex-shrink-0 text-red-500" aria-hidden="true" />
                 <p className="">carolebethuel@wanadoo.fr</p>
             </a>
             <a href="https://www.imdb.com/name/nm1615304/"
                 target="_blank"
-                className="flex space-x-3 text-lg md:text-xl lg:text-2xl ">
+                className="flex space-x-3 text-xl md:text-2xl lg:text-4xl">
                 <img src={imdb} alt="" className='h-10 animate-pulse'/>
                 <p className="">CV</p>
             </a>
