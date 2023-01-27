230c5506f82ab2b65a25cae72ab4faffea7c5fdd
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-27 11:46:31 +0100

    <Route path='*' element={<Home />} />

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Route path= "/" element={<Home />} />
         <Route path= "/films" element={<Home />} />
         <Route path= "/films/:title" element={<Home />} />
+        <Route path='*' element={<Home />} />
         <Route path= "/affiches" element={<Affiches />} />
         <Route path= "/contact" element={<Contact />} />
         <Route path= "/mentionslegales" element={<MentionsLegales />} />
